a4edc37a7d20026a838e115e77f0d92c69c85704
acassiomonteiro <acassiomonteirodev@gmail.com>
2025-11-05 00:01:44 -0300

    feat(auth): configura redirects de autenticação
    - Define LOGIN_URL='login'
    - Define LOGIN_REDIRECT_URL='dashboard'
    - Define LOGOUT_REDIRECT_URL='login'
    - Garante fluxo de autenticação consistente

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Authentication settings
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
